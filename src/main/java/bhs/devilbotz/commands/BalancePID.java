08e9220f9d9decb085d7b86c6ddc68e9c44f9315
Nilesh Agarwalla <49494444+BrownGenius@users.noreply.github.com>
2023-01-22 19:41:33 -0500

    Update src/main/java/bhs/devilbotz/commands/BalancePID.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/bhs/devilbotz/commands/BalancePID.java
+++ src/main/java/bhs/devilbotz/commands/BalancePID.java
@@ -19,12 +19,9 @@ public class BalancePID extends CommandBase {
   {
     // Use addRequirements() here to declare subsystem dependencies.
     m_drive = drive;
-    pid = new 
-      PIDController(
-        0.07, 0, 0.01
-      );
-    addRequirements(
-      drive);}
+    pid = new PIDController(0.07, 0, 0.01);
+    addRequirements(drive);
+  }
 
   // Called when the command is initially scheduled.
   @Override
