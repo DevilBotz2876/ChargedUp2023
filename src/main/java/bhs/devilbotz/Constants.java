888a72874677b037838c6a232d3d733596d2c055
ParkerMeyers <parker@parkermeyers.com>
2023-01-25 19:08:00 -0500

    Spotless Apply

--- src/main/java/bhs/devilbotz/Constants.java
+++ src/main/java/bhs/devilbotz/Constants.java
@@ -15,8 +15,9 @@ package bhs.devilbotz;
  */
 public final class Constants {
 
-    public static final int JOYSTICK_PORT = 0;
-    public static class OperatorConstants {
-        public static final int DRIVER_CONTROLLER_PORT = 0;
-    }
+  public static final int JOYSTICK_PORT = 0;
+
+  public static class OperatorConstants {
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+  }
 }
