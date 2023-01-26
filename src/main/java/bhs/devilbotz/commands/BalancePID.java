3cb7d53667c463617c624f4390405527eb5ca634
Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
2023-01-25 19:35:23 -0500

    Update BalancePID.java

--- src/main/java/bhs/devilbotz/commands/BalancePID.java
+++ src/main/java/bhs/devilbotz/commands/BalancePID.java
@@ -55,6 +55,6 @@ public class BalancePID extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return Utils.isTrueForX(pid.atSetpoint(), 1.5);
+    return false;
   }
 }
