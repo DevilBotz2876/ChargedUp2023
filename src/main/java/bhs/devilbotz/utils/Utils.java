66d95eb5ba23d0575d152b13e1cb4bfb666d7412
Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
2023-01-25 19:26:48 -0500

    Update src/main/java/bhs/devilbotz/utils/Utils.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/bhs/devilbotz/utils/Utils.java
+++ src/main/java/bhs/devilbotz/utils/Utils.java
@@ -3,17 +3,15 @@ package bhs.devilbotz.utils;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Utils {
-    public static boolean isTrueForX(boolean input, double x) {
-        Timer timer = new Timer();
-        if (input) {
-            System.out.println("Timer Started");
-            timer.start();
-        } else {
-            System.out.println("Timer reset");
-            timer.stop();
-            timer.reset();
-        }
-        return timer.get() >= x;
+  public static boolean isTrueForX(boolean input, double x) {
+    Timer timer = new Timer();
+    if (input) {
+      System.out.println("Timer Started");
+      timer.start();
+    } else {
+      System.out.println("Timer reset");
+      timer.stop();
+      timer.reset();
     }
     return timer.get() >= x;
   }
