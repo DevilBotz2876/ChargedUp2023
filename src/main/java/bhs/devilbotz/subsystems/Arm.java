8c9fb90f43777b5aa99596c77a71652edff07645
firydragon57 <92382409+firydragon57@users.noreply.github.com>
2023-01-25 20:41:32 -0500

    Arm subsystem
    Added arm subsystem and commands that move the arm up and down. The up and down commands will obviously change but for right now they are ment to test the arm's functionality (motion)

---
+++ src/main/java/bhs/devilbotz/subsystems/Arm.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package bhs.devilbotz.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import bhs.devilbotz.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+  /** Creates a new Arm. */
+  private WPI_TalonSRX armMotor;
+  private boolean armUp;
+  
+  public Arm() {
+    armMotor = new WPI_TalonSRX(Constants.ARM_CAN_ID);
+    armUp = false;
+  }
+
+  public boolean isArmUp() {
+    return armUp;
+  }
+  
+  public void Up(double speed) {
+    armMotor.set(speed);
+    armUp = true;
+  }
+
+  public void Down(double speed) {
+    armMotor.set(-speed);
+    armUp = false;
+  }
+
+  public void stop() {
+    armMotor.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
