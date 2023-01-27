c3d5f648ebe273cb6969a8acba2d0451645e8fbc
Nilesh Agarwalla <49494444+BrownGenius@users.noreply.github.com>
2023-01-27 11:22:49 -0500

    Removed unused SlewRateLimiter. Created DriveConstants. (#29)
    * Removed unused SlewRateLimiter. Created DriveConstants.

--- src/main/java/bhs/devilbotz/Constants.java
+++ src/main/java/bhs/devilbotz/Constants.java
@@ -24,4 +24,11 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int DRIVER_CONTROLLER_PORT = 0;
   }
+
+  public static final class DriveConstants {
+    public static final int MOTOR_LEFT_MASTER_CAN_ID = 3;
+    public static final int MOTOR_RIGHT_MASTER_CAN_ID = 4;
+    public static final int MOTOR_LEFT_FOLLOWER_CAN_ID = 1;
+    public static final int MOTOR_RIGHT_FOLLOWER_CAN_ID = 2;
+  }
 }
