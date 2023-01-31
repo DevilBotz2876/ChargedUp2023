0843497fe8a19cdbe5d664cd88f6d7d4ea5daa59
Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
2023-01-30 19:23:29 -0500

    Update src/main/java/bhs/devilbotz/subsystems/DriveTrain.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/bhs/devilbotz/subsystems/DriveTrain.java
+++ src/main/java/bhs/devilbotz/subsystems/DriveTrain.java
@@ -4,7 +4,6 @@ import bhs.devilbotz.Constants;
 import bhs.devilbotz.Constants.DriveConstants;
 import bhs.devilbotz.RobotContainer;
 import bhs.devilbotz.utils.ShuffleboardManager;
-import bhs.devilbotz.Constants.DriveConstants;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
