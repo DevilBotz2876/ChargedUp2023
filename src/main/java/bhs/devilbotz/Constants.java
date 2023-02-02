11a494b8ca6f8cb7ba7062e7d72d47cc1f70f2a6
Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
2023-02-01 22:17:54 -0500

    Update src/main/java/bhs/devilbotz/Constants.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/bhs/devilbotz/Constants.java
+++ src/main/java/bhs/devilbotz/Constants.java
@@ -50,7 +50,6 @@ public final class Constants {
     public static final int DRIVER_CONTROLLER_PORT = 0;
   }
 
-
   public static class ArmConstants {
     public static final int ARM_CAN_ID = 5;
   }
