c3fc56af1966ca9df0bf8ab5566a7717b9c9c109
Nilesh Agarwalla <49494444+BrownGenius@users.noreply.github.com>
2023-02-10 10:56:22 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/bhs/devilbotz/lib/AutonomousModes.java
+++ src/main/java/bhs/devilbotz/lib/AutonomousModes.java
@@ -3,11 +3,14 @@ package bhs.devilbotz.lib;
 /**
  * Autonomous modes enum for the autonomous chooser on the dashboard.
  *
- * @see <a href=https://github.com/BHSRobotix/ChargedUp2023#desired-autonomous-routines>Desired Autonomous Routines</a>
+ * @see <a href=https://github.com/BHSRobotix/ChargedUp2023#desired-autonomous-routines>Desired
+ *     Autonomous Routines</a>
  * @since 1/18/2023
  */
 public enum AutonomousModes {
-  /** Don't do anything in autonomous mode. Sit still and let the other alliance members do the work. */
+  /**
+   * Don't do anything in autonomous mode. Sit still and let the other alliance members do the work.
+   */
   SIT_STILL,
   /** Drive straight forwards towards the middle of the field */
   MOBILITY,
@@ -21,7 +24,10 @@ public enum AutonomousModes {
   SCORE_DOCK_AND_ENGAGE,
   /** Perform {@link #SCORE_AND_MOBILITY} and then {@link #DOCK_AND_ENGAGE} */
   SCORE_MOBILITY_DOCK_ENGAGE,
-  /** Perform {@link #SCORE_AND_MOBILITY}, pick up a game piece at center field, and then {@link #DOCK_AND_ENGAGE} */ 
+  /**
+   * Perform {@link #SCORE_AND_MOBILITY}, pick up a game piece at center field, and then {@link
+   * #DOCK_AND_ENGAGE}
+   */
   SCORE_MOBILITY_PICK_DOCK_ENGAGE,
 
   /** Backup short moves the robot backwards a short distance and then stops. */
