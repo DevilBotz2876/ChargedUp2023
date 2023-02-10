a5bac964e766d962731dd37c74c0271f780acebc
joshuman <120066510+joshuman@users.noreply.github.com>
2023-02-10 10:23:46 -0500

    Added javaDocs and Constants (#57)
    * Added javaDocs and Constants
    
    * Ran SpotlessApply
    
    * Update src/main/java/bhs/devilbotz/commands/DriveStraightPID.java
    
    renamed variable to camelCase
    
    Co-authored-by: Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
    
    * Update src/main/java/bhs/devilbotz/commands/DriveStraightPID.java
    
    renamed variable to camelCase
    
    Co-authored-by: Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
    
    * Update src/main/java/bhs/devilbotz/commands/DriveSetDistancePID.java
    
    renamed variable to camelCase
    
    Co-authored-by: Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
    
    * Update src/main/java/bhs/devilbotz/commands/DriveSetDistancePID.java
    
    renamed variable to camelCase
    
    Co-authored-by: Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>
    
    * Made Drive Set Distance PID and Drive Straight PID
    variables camel case.
    
    ---------
    
    Co-authored-by: Parker Meyers <74273030+ParkerMeyers@users.noreply.github.com>

--- src/main/java/bhs/devilbotz/commands/DriveStraightPID.java
+++ src/main/java/bhs/devilbotz/commands/DriveStraightPID.java
@@ -4,22 +4,31 @@
 
 package bhs.devilbotz.commands;
 
+import bhs.devilbotz.Constants;
 import bhs.devilbotz.subsystems.DriveTrain;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/** This command is a PID controller that drives the robot straight to a set distance. */
 public class DriveStraightPID extends CommandBase {
   private DriveTrain drivetrain;
-  private PIDController distance_pid;
-  private PIDController straight_pid;
+  private PIDController distancePid;
+  private PIDController straightPid;
   private double distance;
-
+  /**
+   * The constructor for the Drive Straight PID command.
+   *
+   * @param drivetrain The drive train subsystem.
+   * @param distance The distance (in meters) the robot needs to cover.
+   */
   public DriveStraightPID(DriveTrain drivetrain, double distance) {
     this.drivetrain = drivetrain;
     this.distance = distance;
-    distance_pid = new PIDController(0.25, 0, 0.1);
-    straight_pid = new PIDController(0.1, 0, 0);
+    distancePid =
+        new PIDController(Constants.DISTANCE_P, Constants.DISTANCE_I, Constants.DISTANCE_K);
+    straightPid =
+        new PIDController(Constants.STRAIGHT_P, Constants.STRAIGHT_I, Constants.STRAIGHT_K);
 
     addRequirements(drivetrain);
     // Use addRequirements() here to declare subsystem dependencies.
@@ -36,14 +45,14 @@ public class DriveStraightPID extends CommandBase {
   @Override
   public void execute() {
     // distance_pid.setTolerance(1);
-    double output = distance_pid.calculate(drivetrain.getAverageDistance(), distance);
-    double turnError = straight_pid.calculate(drivetrain.getYaw(), 0);
+    double output = distancePid.calculate(drivetrain.getAverageDistance(), distance);
+    double turnError = straightPid.calculate(drivetrain.getYaw(), 0);
     drivetrain.arcadeDrive(output, -turnError);
 
     SmartDashboard.putNumber("Distance output", output);
-    SmartDashboard.putNumber("Position Tolerance", distance_pid.getPositionTolerance());
-    SmartDashboard.putBoolean("at Setpoint", distance_pid.atSetpoint());
-    SmartDashboard.putNumber("Position Error", distance_pid.getPositionError());
+    SmartDashboard.putNumber("Position Tolerance", distancePid.getPositionTolerance());
+    SmartDashboard.putBoolean("at Setpoint", distancePid.atSetpoint());
+    SmartDashboard.putNumber("Position Error", distancePid.getPositionError());
     SmartDashboard.putNumber("Distance", drivetrain.getAverageDistance());
     SmartDashboard.putNumber("Turn output", turnError);
     SmartDashboard.putNumber("Yaw", drivetrain.getYaw());
@@ -60,7 +69,7 @@ public class DriveStraightPID extends CommandBase {
   @Override
   public boolean isFinished() {
     // return distance_pid.atSetpoint();
-    if (Math.abs(distance_pid.getPositionError()) < 0.001) {
+    if (Math.abs(distancePid.getPositionError()) < 0.001) {
       return true;
     }
     return false;
