a63b1b74227a420f14d5b2940817d5353b249989
Nilesh Agarwalla <nilesh@alum.mit.edu>
2023-02-10 20:00:07 -0500

    spotlessApply

--- src/main/java/bhs/devilbotz/subsystems/DriveTrain.java
+++ src/main/java/bhs/devilbotz/subsystems/DriveTrain.java
@@ -19,7 +19,6 @@ import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
