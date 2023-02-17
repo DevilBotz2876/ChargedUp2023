4ad29998be2aa2e848738465e06a62698b5ba7d9
ParkerMeyers <parker@parkermeyers.com>
2023-02-17 12:08:09 -0500

    Spotless Apply

--- src/main/java/bhs/devilbotz/subsystems/Gripper.java
+++ src/main/java/bhs/devilbotz/subsystems/Gripper.java
@@ -5,11 +5,9 @@
 package bhs.devilbotz.subsystems;
 
 import bhs.devilbotz.Constants;
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticHub;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
@@ -19,8 +17,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * @author joshuamanoj &amp; ParkerMeyers
  */
 public class Gripper extends SubsystemBase {
-  private static final PneumaticHub pneumaticHub = new PneumaticHub(Constants.GripperConstants.COMPRESSOR_CAN_ID);
-  private final DoubleSolenoid doubleSolenoid = pneumaticHub.makeDoubleSolenoid(Constants.GripperConstants.GRIPPER_SOLENOID_FORWARD, Constants.GripperConstants.GRIPPER_SOLENOID_REVERSE);
+  private static final PneumaticHub pneumaticHub =
+      new PneumaticHub(Constants.GripperConstants.COMPRESSOR_CAN_ID);
+  private final DoubleSolenoid doubleSolenoid =
+      pneumaticHub.makeDoubleSolenoid(
+          Constants.GripperConstants.GRIPPER_SOLENOID_FORWARD,
+          Constants.GripperConstants.GRIPPER_SOLENOID_REVERSE);
 
   /** The constructor for the gripper subsystem. */
   public Gripper() {
@@ -61,7 +63,7 @@ public class Gripper extends SubsystemBase {
   /**
    * Returns true if the compressor pressure has reached the set value. The set value is controlled
    * physically on the robot
-   * 
+   *
    * @return true if pressue is at pre-configured set point
    */
   public static boolean getAtSetpoint() {
