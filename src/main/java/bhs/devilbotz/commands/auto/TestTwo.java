510fcdb2f7b04ca6efa425ffd06fa7d5332b1fd9
rohit18-cool <rohitvijayakumar09@gmail.com>
2023-02-18 14:52:14 -0500

    increase speed

--- src/main/java/bhs/devilbotz/commands/auto/TestTwo.java
+++ src/main/java/bhs/devilbotz/commands/auto/TestTwo.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class TestTwo extends SequentialCommandGroup {
   // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a
   // max acceleration of 3 m/s^2
-  PathPlannerTrajectory testPath = PathPlanner.loadPath("Test Two", new PathConstraints(1, 0.5));
+  PathPlannerTrajectory testPath = PathPlanner.loadPath("Test Two", new PathConstraints(2, 1));
 
   /**
    * The constructor for the balance auto command.
