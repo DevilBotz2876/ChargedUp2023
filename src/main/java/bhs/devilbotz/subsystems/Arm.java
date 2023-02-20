98008b77f2dcbc619da669a85108def93177a6f8
Maciej Wiczynski <loafjunk@loafdog.org>
2023-02-20 09:22:54 -0500

    Add comments/docs to arm subsystems

--- src/main/java/bhs/devilbotz/subsystems/Arm.java
+++ src/main/java/bhs/devilbotz/subsystems/Arm.java
@@ -14,10 +14,49 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
- * This subsystem controls the gripper.
+ * This subsystem controls arm.
  *
- * @since 1/25/2023
- * @author joshuamanoj &amp; ParkerMeyers
+ * <p>The arm must start matches fully inside the robot chassis. The arm moves up using two (gas?)
+ * struts. It is lowered by a motor turning a winch. A rope winds around the winch pulling the arm
+ * down. In this design it is not possible to move the arm too far up and break it. But it is
+ * possible for the rope around the winch to unwind completely and start winding itself again in the
+ * opposite direction. This will result in 'up' commands moving the arm down (and vice versa). It is
+ * also possible for the rope to become tangled around the winch/shaft if there is enough slack or
+ * extra rope.
+ *
+ * <p>It is possible to break the arm if it is lowered too far. The motor will be winding up the
+ * rope and pulling the arm down into the chassis. The arm will hit a hard stop and the motor will
+ * be pulling the arm against this hard stop. It is important therefore to detect when the arm
+ * reaches top most position bottom most position. We will use limit switches to do this.
+ *
+ * <p>There will also be an encoder mounted on the shoulder joint. Using an encoder on the shoulder
+ * joint instead of on the motor or winch shaft should provide more accurate measurement of the arm
+ * position. Measuring the rotations of the winch might be less accurate because the rope is winding
+ * around it in an inconsistent manner. Maybe this is neglible?
+ *
+ * <p>There are methods to move the arm up and down at a set speed. We do not intend to vary the
+ * speed of the arm or expose speed control outside the arm subsystem. There are methods to detect
+ * when arm reaches the top and bottom most positions. The intent of these methods is to use them in
+ * arm Commands to move the arm up and down but stop when the top and bottom most positions are
+ * reached.
+ *
+ * <p>The encoder is not needed to do any control of the arm moving up or down. It can be used to
+ * move the arm to known positions/heights. For example if we plan on picking up pieces from the
+ * portal in the substation we should provide a Command that can move the arm to the correct height
+ * to pickup cone/cube. We will need to determine what distance/position encoder reads to be at
+ * correct height.
+ *
+ * <p>There is a method to detect a 'middle' position, move a set distance, and to get the current
+ * position. The idea here is to create a set of commands that can move the arm to a set position
+ * just high enough above a low/mid/high grid scoring node to score a cube/cone. Then provide a
+ * second command that can lower the arm and release the gripper to place/score the cube/cone. Not
+ * sure this will be useful to the driver. Will need to experiment with this if time allows.
+ *
+ * <p>There are two limit switches, one at top of range of motion and one at bottom of range of
+ * motion. The limit switches are plugged into the roborio digital input/output ports.
+ *
+ * <p>There is a quadrature encoder on the arm joint. The signal and power lines are plugged into
+ * the roborio digital input/output ports.
  */
 public class Arm extends SubsystemBase {
   private final CANSparkMax armMotor;
@@ -76,14 +115,23 @@ public class Arm extends SubsystemBase {
     armMotor.set(speed);
   }
 
+  /** Move the arm up at set speed. There is no check/protection against moving arm too far up. */
   public void up() {
     armMotor.set(1);
   }
 
+  /**
+   * Move the arm down at set speed. There is no check/protection against moving arm too far down.
+   */
   public void down() {
     armMotor.set(-1);
   }
 
+  /**
+   * Check if arm is at bottom most position.
+   *
+   * @return true if at bottom, false if not.
+   */
   public boolean atBottom() {
     // TODO: when arm is fixed change this to check bottom limit switch
     //
@@ -91,14 +139,13 @@ public class Arm extends SubsystemBase {
     return encoder.getDistance() >= 400;
   }
 
+  /**
+   * Check if arm is at top most position.
+   *
+   * @return true if at top, false if not.
+   */
   public boolean atTop() {
     return isTopLimit();
-  }
-
-  public boolean atMiddle() {
-    // Just a random number for now.  Need to measure what real/good encoder value is to position
-    // arm right above middle scoring spot.
-    return encoder.getDistance() >= 200 && encoder.getDistance() <= 205;
   }
 
   /** This method stops the arm. */
@@ -107,14 +154,53 @@ public class Arm extends SubsystemBase {
     armMotor.stopMotor();
   }
 
+  /**
+   * Return state of top limit switch
+   *
+   * @return true limit switch is pressed, false if not.
+   */
   public boolean isTopLimit() {
     return !topLimitSwitch.get();
   }
 
+  /**
+   * Return state of bottom limit switch
+   *
+   * @return true limit switch is pressed, false if not.
+   */
   public boolean isBottomLimit() {
     return !bottomLimitSwitch.get();
   }
 
+  /**
+   * Check if arm is at middle position based on encoder position. Middle is just high enough above
+   * middle grid scoring node to allow cone or cube to be lined up.
+   *
+   * @return true if at middle position false if not.
+   */
+  public boolean atMiddle() {
+    // TODO: Just a random number for now.  Need to measure what real/good encoder value is to
+    // position arm right above middle scoring grid node.
+    return encoder.getDistance() >= 200 && encoder.getDistance() <= 205;
+  }
+
+  /**
+   * Check if arm is at correct height to grab cone/cube from substation portal.
+   *
+   * @return true if at substation portal height, false if not.
+   */
+  public boolean atSubstationPortal() {
+    // TODO: Just a random number for now.  Need to measure what real/good encoder value is to
+    // position
+    // arm at height to pickup from portal
+    return encoder.getDistance() >= 200 && encoder.getDistance() <= 205;
+  }
+
+  /**
+   * Return current position of arm.
+   *
+   * @return position of arm.
+   */
   public double getPosition() {
     return encoder.getDistance();
   }
