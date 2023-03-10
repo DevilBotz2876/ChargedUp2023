387ae780334b003b1abae27933fbd6dad64942dc
Nilesh Agarwalla <nilesh@alum.mit.edu>
2023-03-10 08:56:51 -0500

    spotlessApply

--- src/main/java/bhs/devilbotz/commands/arm/ArmSafety.java
+++ src/main/java/bhs/devilbotz/commands/arm/ArmSafety.java
@@ -23,8 +23,9 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
  *             hitting the robot frame
  *       </ul>
  * </ul>
- *       <i>Note: ALL arm commands that move the arm should extend this class and must implement the
- *       following two methods</i>
+ *
+ * <i>Note: ALL arm commands that move the arm should extend this class and must implement the
+ * following two methods</i>
  */
 public abstract class ArmSafety extends CommandBase {
   private final Arm arm;
