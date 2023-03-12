e62c549766b2bc05cc58f72ed25aff88c7ecc83c
Nilesh Agarwalla <nilesh@alum.mit.edu>
2023-03-12 09:28:25 -0400

    Improved DriveStraigthtToDock to use constant speed on approach/climb to the ramp instead of a distance PID

--- src/main/java/bhs/devilbotz/commands/auto/DriveStraightToDock.java
+++ src/main/java/bhs/devilbotz/commands/auto/DriveStraightToDock.java
@@ -7,12 +7,14 @@ package bhs.devilbotz.commands.auto;
 import bhs.devilbotz.Robot;
 import bhs.devilbotz.commands.CommandDebug;
 import bhs.devilbotz.subsystems.DriveTrain;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
  * This command is a PID controller that drives the robot straight to a set distance up a hill to
  * dock with the charging port.
  */
-public class DriveStraightToDock extends DriveStraightPID {
+public class DriveStraightToDock extends CommandBase {
   private enum DockState {
     ON_GROUND,
     ON_RAMP,
@@ -20,16 +22,29 @@ public class DriveStraightToDock extends DriveStraightPID {
   }
 
   private DockState currentState = DockState.ON_GROUND;
+  protected DriveTrain drivetrain;
+  private PIDController straightPid;
+  private final double maxDistance;
+  private double startAngle;
+  private double startDistance;
+
   /**
    * The constructor for the Drive Straight To Dock PID command.
    *
    * @param drivetrain The drive train subsystem.
-   * @param distance The MAX distance (in meters) the robot needs to cover. We end when the distance
-   *     is reached OR we've detected that we are physically on the dock
+   * @param maxDistance The MAX distance (in meters) the robot needs to cover. We end when the
+   *     distance is reached OR we've detected that we are physically on the dock
    */
-  public DriveStraightToDock(DriveTrain drivetrain, double distance) {
-    super(drivetrain, distance);
-    // Use addRequirements() here to declare subsystem dependencies.
+  public DriveStraightToDock(DriveTrain drivetrain, double maxDistance) {
+    this.drivetrain = drivetrain;
+    this.maxDistance = maxDistance;
+    straightPid =
+        new PIDController(
+            Robot.getDriveTrainConstant("STRAIGHT_P").asDouble(),
+            Robot.getDriveTrainConstant("STRAIGHT_I").asDouble(),
+            Robot.getDriveTrainConstant("STRAIGHT_D").asDouble());
+    straightPid.enableContinuousInput(0, 360);
+    addRequirements(drivetrain);
   }
 
   int onRampCount = 0;
@@ -39,20 +54,31 @@ public class DriveStraightToDock extends DriveStraightPID {
   @Override
   public void initialize() {
     CommandDebug.trace();
-    super.initialize();
+    startAngle = drivetrain.getYaw();
+    startDistance = drivetrain.getAverageDistance();
+    CommandDebug.trace(
+        "startRoll: "
+            + drivetrain.getRoll()
+            + "startAngle: "
+            + startAngle
+            + " maxDistance: "
+            + maxDistance);
   }
 
   @Override
   public void execute() {
     double currentRoll = drivetrain.getRoll();
+    double speed;
 
+    CommandDebug.trace("currentRoll: " + currentRoll);
+    
     switch (currentState) {
       case ON_GROUND:
         /* We start in the ON_GROUND state. We see if the currentRoll is within the expected ramp angle.
          * Empirically, the roll is between min/max when on the ramp.
          * When approaching the ramp, we cap the max speed to prevent crashing into the ramp
          */
-        setMaxSpeed(Robot.getDriveTrainConstant("DOCK_MAX_SPEED_ON_GROUND").asDouble(0.75));
+        speed = Robot.getDriveTrainConstant("DOCK_MAX_SPEED_ON_GROUND").asDouble(0.75);
 
         // We use the absolute value of the current roll so that we can approach the ram either
         // going forward or backwards.
@@ -82,7 +108,7 @@ public class DriveStraightToDock extends DriveStraightPID {
          * ramp to teeter quickly to the other side
          * While the roll is decreasing and the currentRoll is less than the empirical ramp roll we assume we are leveling off.
          */
-        setMaxSpeed(Robot.getDriveTrainConstant("DOCK_MAX_SPEED_ON_RAMP").asDouble(0.50));
+        speed = Robot.getDriveTrainConstant("DOCK_MAX_SPEED_ON_RAMP").asDouble(0.50);
         double deltaRoll = currentRoll - previousRoll;
         if ((deltaRoll < 0)
             && (currentRoll < Robot.getDriveTrainConstant("DOCK_MIN_RAMP_ROLL").asDouble(10))) {
@@ -105,30 +131,38 @@ public class DriveStraightToDock extends DriveStraightPID {
         break;
 
       case LEVELING_OFF:
-        setMaxSpeed(0.01);
-        return;
+      default:
+        speed = 0;
+        break;
     }
 
     /* We save the current roll so we can calculate the deltaRoll next time */
     previousRoll = currentRoll;
 
-    /* Execute the base class's execute function to drive straight */
-    super.execute();
+    double turnError = straightPid.calculate(drivetrain.getYaw(), startAngle);
+    drivetrain.arcadeDrive(speed, -turnError);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    CommandDebug.trace();
-    super.end(interrupted);
+    CommandDebug.trace(
+        "endRoll: "
+            + drivetrain.getRoll()
+            + "endAngle: "
+            + drivetrain.getYaw()
+            + " distance: "
+            + (drivetrain.getAverageDistance() - startDistance));
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    double distanceTravelled = drivetrain.getAverageDistance() - startDistance;
     /* We are finished when we are in the LEVELING_OFF state.
      * As a backup, we also assume we are finished when the total distance has been traveled
      */
-    return ((DockState.LEVELING_OFF == currentState) || super.isFinished());
+    return ((DockState.LEVELING_OFF == currentState)
+        || (Math.abs(distanceTravelled) >= Math.abs(maxDistance)));
   }
 }
