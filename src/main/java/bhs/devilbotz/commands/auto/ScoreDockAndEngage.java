a4c7cba296cbde9545ebdc1baf45820f0d54ebb4
ParkerMeyers <parker@parkermeyers.com>
2023-03-18 09:25:20 -0400

    Shorten the times for score dock and engage

--- src/main/java/bhs/devilbotz/commands/auto/ScoreDockAndEngage.java
+++ src/main/java/bhs/devilbotz/commands/auto/ScoreDockAndEngage.java
@@ -39,7 +39,7 @@ public class ScoreDockAndEngage extends SequentialCommandGroup {
 
     addCommands(CommandDebug.start());
     addCommands(new AutoScore(arm, drivetrain, gripper));
-    addCommands(new DriveStraightPID(drivetrain, -DriveConstants.POSITION_DRIVE_FROM_PORTAL, 1));
+    // addCommands(new DriveStraightPID(drivetrain, -DriveConstants.POSITION_DRIVE_FROM_PORTAL, 1));
     addCommands(
         new ParallelCommandGroup(
             // Move arm all the way down
