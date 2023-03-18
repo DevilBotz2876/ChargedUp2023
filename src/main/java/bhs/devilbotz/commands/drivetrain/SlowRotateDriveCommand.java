adff9c2b639bbd8076bff8162f2ea647fbfe6cb1
ParkerMeyers <parker@parkermeyers.com>
2023-03-18 15:48:05 -0400

    Spotless Apply

--- src/main/java/bhs/devilbotz/commands/drivetrain/SlowRotateDriveCommand.java
+++ src/main/java/bhs/devilbotz/commands/drivetrain/SlowRotateDriveCommand.java
@@ -2,11 +2,13 @@ package bhs.devilbotz.commands.drivetrain;
 
 import bhs.devilbotz.Constants;
 import bhs.devilbotz.subsystems.DriveTrain;
-
 import java.util.function.DoubleSupplier;
 
 public class SlowRotateDriveCommand extends DriveCommand {
   public SlowRotateDriveCommand(DriveTrain drive, DoubleSupplier speed, DoubleSupplier rot) {
-    super(drive, speed, () -> rot.getAsDouble() / Constants.DriveConstants.PRECISION_ROTATE_SPEED_DIVIDER);
+    super(
+        drive,
+        speed,
+        () -> rot.getAsDouble() / Constants.DriveConstants.PRECISION_ROTATE_SPEED_DIVIDER);
   }
 }
