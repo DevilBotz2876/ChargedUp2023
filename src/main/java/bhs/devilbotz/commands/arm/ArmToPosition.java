a594e23a6b074f704a351d6a57e3ec0599250f15
Nilesh Agarwalla <49494444+BrownGenius@users.noreply.github.com>
2023-04-08 17:18:13 -0400

    Arm to Position PID + SDE stability improvements (#166)
    * Added PID loop for Arm to Position (PID needs to be tuned)
    
    * fix values
    
    * fix values
    
    * Fix score and driving pid
    
    * spotlessApply
    
    * Revert POSITION_DRIVE_FROM_PORTAL
    
    * Revert "Revert POSITION_DRIVE_FROM_PORTAL"
    
    This reverts commit a2576ad97189ff96ee8386ed1dd6752efdfafbbf.
    
    * add delay back
    
    * revert addition changes
    
    ---------
    
    Co-authored-by: ParkerMeyers <parker@parkermeyers.com>

--- src/main/java/bhs/devilbotz/commands/arm/ArmToPosition.java
+++ src/main/java/bhs/devilbotz/commands/arm/ArmToPosition.java
@@ -4,13 +4,19 @@ import bhs.devilbotz.Constants.ArmConstants;
 import bhs.devilbotz.commands.CommandDebug;
 import bhs.devilbotz.subsystems.Arm;
 import bhs.devilbotz.subsystems.Gripper;
+import edu.wpi.first.math.controller.PIDController;
 
 public class ArmToPosition extends ArmSafety {
   private final double targetPosition;
+  private PIDController positionPid;
 
   public ArmToPosition(Arm arm, double targetPosition, Gripper gripper) {
     super(arm, gripper);
     this.targetPosition = targetPosition;
+    positionPid =
+        new PIDController(
+            ArmConstants.POSITION_P, ArmConstants.POSITION_I, ArmConstants.POSITION_D);
+    positionPid.setTolerance(ArmConstants.POSITION_TOLERANCE);
   }
 
   // Called when the command is initially scheduled.
@@ -23,20 +29,17 @@ public class ArmToPosition extends ArmSafety {
   @Override
   public void executeWithSafety() {
     double currentPosition = getPosition();
-
-    if (currentPosition < targetPosition) {
-      up();
-    } else if (currentPosition > targetPosition) {
-      down();
+    double speed = positionPid.calculate(currentPosition, targetPosition) / 600;
+    if (speed > 0) {
+      up(speed);
+    } else if (speed < 0) {
+      down(speed);
     }
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinishedWithSafety() {
-    double currentPosition = getPosition();
-
-    return (currentPosition >= targetPosition)
-        && (currentPosition < targetPosition + ArmConstants.POSITION_TOLERANCE);
+    return positionPid.atSetpoint();
   }
 }
